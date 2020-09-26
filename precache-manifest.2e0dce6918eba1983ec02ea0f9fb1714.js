b5a722dc754c24786aa2491c3ff078f984fe3f03
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-09-26 07:15:37 -0400

    Updates

---
+++ precache-manifest.2e0dce6918eba1983ec02ea0f9fb1714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c763b1447abf7b40c4676f7352ce55",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "7a4883e7afe278300079",
+    "url": "/Employee-Directory/static/css/main.74dd86cd.chunk.css"
+  },
+  {
+    "revision": "c6d9f1cf270b0d1f17e9",
+    "url": "/Employee-Directory/static/js/2.482e7884.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.482e7884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4883e7afe278300079",
+    "url": "/Employee-Directory/static/js/main.02fce2cc.chunk.js"
+  },
+  {
+    "revision": "52e1e8e23e7dfaf64514",
+    "url": "/Employee-Directory/static/js/runtime-main.6ca9f082.js"
+  }
+]);
